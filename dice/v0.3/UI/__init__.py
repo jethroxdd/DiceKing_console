fd3b40bc7950de6cb33da41da8d4453e5f276d58
Jethro <56423687+jethroxdd@users.noreply.github.com>
2025-04-09 17:01:51 +0300

    Delete dice/v0.3/UI directory

--- dice/v0.3/UI/__init__.py
+++
@@ -1,7 +0,0 @@
-from UI.utility import *
-from UI.color import *
-
-class msg:
-    incorrect_input = Paint("Incorrect input!", 88)
-    rune_attached = Paint("Rune attached", 220)
-    rune_replaced = Paint("Rune replaced", 220)
