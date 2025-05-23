23e381090b0476cd32d269aa51bbf04177f33f99
Jethro <56423687+jethroxdd@users.noreply.github.com>
2025-05-23 14:25:31 +0300

    Delete dice/v0.3/core/effect/effect.py

--- dice/v0.3/core/effect/effect.py
+++
@@ -1,6 +0,0 @@
-class BaseEffect:
-    def __init__(self, name, value, duration, order):
-        self.name = name
-        self.value = value
-        self.duration = duration
-        self.order = order
