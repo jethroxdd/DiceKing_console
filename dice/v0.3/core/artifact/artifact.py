49558778830b7d515d9ed5c5633965cdd8998b91
Jethro <56423687+jethroxdd@users.noreply.github.com>
2025-05-23 14:25:04 +0300

    Delete dice/v0.3/core/artifact/artifact.py

--- dice/v0.3/core/artifact/artifact.py
+++
@@ -1,8 +0,0 @@
-class BaseArtifact:
-    def __init__(self, player, name, description):
-        self.player = player
-        self.name = name
-        self.description = description
-    
-    def __str__(self):
-        return f"{self.name} - {self.description}"
